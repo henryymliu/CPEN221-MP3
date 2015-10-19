62d798893484349c4cbdea8e85c948d82dc8dfd9
Henry Liu <smilingcrash@gmail.com>
2015-10-18 23:15:53 -0700

    implemented DFS

--- src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
+++ src/ca/ubc/ece/cpen221/mp3/graph/Algorithms.java
@@ -1,6 +1,7 @@
 package ca.ubc.ece.cpen221.mp3.graph;
 
 import ca.ubc.ece.cpen221.mp3.staff.Graph;
+import java.util.*;
 import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 public class Algorithms {
@@ -26,6 +27,29 @@ public class Algorithms {
 	public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
 		// TODO: Implement this method and others
 		return 0;
+	}
+
+	public static List<Vertex> DepthFirstSearch(Graph graph, Vertex v) {
+		HashMap<Vertex, Boolean> discoveredVertices = new LinkedHashMap<Vertex, Boolean>();
+		List<Vertex> traversedVertices = new LinkedList<Vertex>();
+		Stack<Vertex> vertexStack = new Stack<Vertex>();
+		vertexStack.push(v);
+
+		while (!vertexStack.isEmpty()) {
+			Vertex n = vertexStack.pop();
+			if (!discoveredVertices.get(v)) {
+				discoveredVertices.put(v, true);
+				for(Vertex neighbour: graph.getDownstreamNeighbors(v)){
+					vertexStack.push(neighbour);
+				}
+			}
+		}
+		for(Vertex discovered: discoveredVertices.keySet()){
+			if(discoveredVertices.get(discovered)){
+				traversedVertices.add(discovered);
+			}
+		}
+		return new LinkedList<Vertex>(traversedVertices);
 	}
 
 }
---
+++ src/ca/ubc/ece/cpen221/mp3/tests/ListGraphTest.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package ca.ubc.ece.cpen221.mp3.tests;
+import ca.ubc.ece.cpen221.mp3.graph.*;
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Henry Liu
+ *
+ */
+public class ListGraphTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		AdjacencyListGraph G = new AdjacencyListGraph();
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
