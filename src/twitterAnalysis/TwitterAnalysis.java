fd5534ebfa77ebde7acebe1ed23c92c4a73102a0
hcoopos <henryymliu@gmail.com>
2015-10-22 17:11:02 -0700

    began implementation of twitter analysis

--- src/twitterAnalysis/TwitterAnalysis.java
+++ src/twitterAnalysis/TwitterAnalysis.java
@@ -1,5 +1,15 @@
 package twitterAnalysis;
 
 public class TwitterAnalysis {
-
+	public static void main(String[] args){
+		
+		final int ARGS_SIZE = 2;
+		if(args.length < ARGS_SIZE){
+			System.out.println("Not enough input arguments.");
+			return;
+		}
+		String fileName = args[0];
+		String outFile = args[1];
+		
+	}
 }
