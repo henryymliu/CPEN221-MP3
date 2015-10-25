1f724b5949685eb3a1def30d7abc3d0d7592203b
Jamie Ye <yjamie96@gmail.com>
2015-10-25 16:48:15 -0700

    Added some test cases

--- src/ca/ubc/ece/cpen221/mp3/tests/AlgorithmTest.java
+++ src/ca/ubc/ece/cpen221/mp3/tests/AlgorithmTest.java
@@ -2,9 +2,8 @@ package ca.ubc.ece.cpen221.mp3.tests;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.sql.*;
+import java.util.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,8 +14,10 @@ import ca.ubc.ece.cpen221.mp3.graph.Algorithms;
 import ca.ubc.ece.cpen221.mp3.staff.Vertex;
 
 public class AlgorithmTest {
-    AdjacencyListGraph G;
-    AdjacencyMatrixGraph M;
+    AdjacencyListGraph G1;
+    AdjacencyMatrixGraph M1;
+    AdjacencyListGraph G2;
+    AdjacencyMatrixGraph M2;
 
     Vertex a = new Vertex("a");
     Vertex b = new Vertex("b");
@@ -31,201 +32,161 @@ public class AlgorithmTest {
 
     @Before
     public void setupGraph() {
-        G = new AdjacencyListGraph();
-        G.addVertex(a);
-        G.addVertex(b);
-        G.addVertex(c);
-        G.addVertex(d);
-        G.addVertex(e);
-        G.addVertex(f);
-        G.addVertex(g);
-        G.addVertex(h);
-        G.addVertex(i);
-        G.addVertex(j);
+        G1 = new AdjacencyListGraph();
+        G1.addVertex(a);
+        G1.addVertex(b);
+        G1.addVertex(c);
+        G1.addVertex(d);
+        G1.addVertex(e);
+        G1.addVertex(f);
+        G1.addVertex(g);
+        G1.addVertex(h);
+        G1.addVertex(i);
+        G1.addVertex(j);
+        
+        G1.addEdge(a, a);
+        G1.addEdge(a, b);
+        G1.addEdge(b, c);
+        G1.addEdge(c, e);
+        G1.addEdge(c, f);
+        G1.addEdge(d, i);
+        G1.addEdge(e, c);
+        G1.addEdge(i, j);
+        G1.addEdge(j, e);
+        G1.addEdge(j, a);
 
-        M = new AdjacencyMatrixGraph();
-        M.addVertex(a);
-        M.addVertex(b);
-        M.addVertex(c);
-        M.addVertex(d);
-        M.addVertex(e);
-        M.addVertex(f);
-        M.addVertex(g);
-        M.addVertex(h);
-        M.addVertex(i);
-        M.addVertex(j);
-
-        /*
-         * G.addEdge(b, c); G.addEdge(c, a);
-         */
+        M1 = new AdjacencyMatrixGraph();
+        M1.addVertex(a);
+        M1.addVertex(b);
+        M1.addVertex(c);
+        M1.addVertex(d);
+        M1.addVertex(e);
+        M1.addVertex(f);
+        M1.addVertex(g);
+        M1.addVertex(h);
+        M1.addVertex(i);
+        M1.addVertex(j);
+        
+        M1.addEdge(a, a);
+        M1.addEdge(a, b);
+        M1.addEdge(b, c);
+        M1.addEdge(c, e);
+        M1.addEdge(c, f);
+        M1.addEdge(d, i);
+        M1.addEdge(i, j);
+        M1.addEdge(j, e);
+        M1.addEdge(j, a);
+        
+        G2 = new AdjacencyListGraph();
+        G2.addVertex(a);
+        G2.addVertex(b);
+        G2.addVertex(c);
+        G2.addVertex(d);
+        G2.addVertex(e);
+        G2.addVertex(f);
+        G2.addVertex(g);
+        G2.addVertex(h);
+        G2.addVertex(i);
+        G2.addVertex(j);
+        G2.addEdge(a, c);
+        G2.addEdge(a, f);
+        G2.addEdge(b, c);
+        G2.addEdge(c, e);
+        G2.addEdge(d, c);
+        G2.addEdge(f, h);
+        G2.addEdge(f, i);
+        G2.addEdge(g, j);
+        
+        M2 = new AdjacencyMatrixGraph();
+        M2.addVertex(a);
+        M2.addVertex(b);
+        M2.addVertex(c);
+        M2.addVertex(d);
+        M2.addVertex(e);
+        M2.addVertex(f);
+        M2.addVertex(g);
+        M2.addVertex(h);
+        M2.addVertex(i);
+        M2.addVertex(j);
+        M2.addEdge(a, c);
+        M2.addEdge(a, f);
+        M2.addEdge(b, c);
+        M2.addEdge(c, e);
+        M2.addEdge(d, c);
+        M2.addEdge(f, h);
+        M2.addEdge(f, i);
+        M2.addEdge(g, j);
     }
 
     @Test
     public void testShortestDistance() {
 
         // for AdjacencyListGraph
-        G.addEdge(a, b);
-        G.addEdge(b, c);
-        G.addEdge(c, d);
-        G.addEdge(d, e);
-        G.addEdge(e, f);
-        G.addEdge(f, g);
-        G.addEdge(g, h);
-        G.addEdge(h, i);
-        G.addEdge(i, j);
-        G.addEdge(j, a);
 
-        G.addEdge(a, f);
-        G.addEdge(d, e);
-        G.addEdge(a, a);
-
-        assertEquals((Algorithms.shortestDistance(G, a, e)), 4);
-
-        assertEquals((Algorithms.shortestDistance(G, a, f)), 1);
-        assertEquals((Algorithms.shortestDistance(G, a, j)), 5);
-        assertEquals((Algorithms.shortestDistance(G, a, a)), 1);
-        assertEquals((Algorithms.shortestDistance(G, e, e)), 10);
+        assertEquals((Algorithms.shortestDistance(G1, a, e)), 3);
+        assertEquals((Algorithms.shortestDistance(G1, a, f)), 3);
+        assertEquals((Algorithms.shortestDistance(G1, a, a)), 1);
+        assertEquals((Algorithms.shortestDistance(G1, d, j)), 2);
+        assertEquals((Algorithms.shortestDistance(G1, a, j)), -1);
+        assertEquals((Algorithms.shortestDistance(G1, e, d)), -1);
+        assertEquals((Algorithms.shortestDistance(G1, j, j)), -1);
 
         // for AdjacencyMatrixGraph
-        M.addEdge(a, b);
-        M.addEdge(b, c);
-        M.addEdge(c, d);
-        M.addEdge(d, e);
-        M.addEdge(e, f);
-        M.addEdge(f, g);
-        M.addEdge(g, h);
-        M.addEdge(h, i);
-        M.addEdge(i, j);
-        M.addEdge(j, a);
 
-        M.addEdge(a, f);
-        M.addEdge(d, e);
-        M.addEdge(a, a);
-
-        assertEquals((Algorithms.shortestDistance(M, a, e)), 4);
-
-        assertEquals((Algorithms.shortestDistance(M, a, f)), 1);
-        assertEquals((Algorithms.shortestDistance(M, a, j)), 5);
-        assertEquals((Algorithms.shortestDistance(M, a, a)), 1);
-        assertEquals((Algorithms.shortestDistance(M, e, e)), 10);
-
+        assertEquals((Algorithms.shortestDistance(M1, a, e)), 3);
+        assertEquals((Algorithms.shortestDistance(M1, a, f)), 3);
+        assertEquals((Algorithms.shortestDistance(M1, a, a)), 1);
+        assertEquals((Algorithms.shortestDistance(M1, d, j)), 2);
+        assertEquals((Algorithms.shortestDistance(M1, a, j)), -1);
+        assertEquals((Algorithms.shortestDistance(M1, e, d)), -1);
+        assertEquals((Algorithms.shortestDistance(M1, j, j)), -1);
+        
     }
 
     @Test
     public void testBreadthFirstSearch() {
-        G.addEdge(a, b);
-        G.addEdge(b, d);
-        G.addEdge(d, g);
-        G.addEdge(g, c);
-        G.addEdge(d, f);
-        G.addEdge(f, a);
-        G.addEdge(e, h);
-        G.addEdge(h, i);
-        G.addEdge(i, h);
-        G.addEdge(h, j);
-
-        // assertEquals(new HashSet<List<Vertex>>(),
-        // Algorithms.DepthFirstSearch(G));
+        HashSet<List<Vertex>> listBreadthFirstSearch = new HashSet<List<Vertex>>();
+        List<Vertex> arrayA = new ArrayList<Vertex>();
+        
         // assertEquals(new HashSet<List<Vertex>>(), Algorithms.breadthFirstSearch(G));
 
-        M.addEdge(a, b);
-        M.addEdge(b, d);
-        M.addEdge(d, g);
-        M.addEdge(g, c);
-        M.addEdge(d, f);
-        M.addEdge(f, a);
-        M.addEdge(e, h);
-        M.addEdge(h, i);
-        M.addEdge(i, h);
-        M.addEdge(h, j);
-        // assertEquals(new HashSet<List<Vertex>>(), Algorithms.breadthFirstSearch(M));
+        
+        //assertEquals(new HashSet<List<Vertex>>(), Algorithms.breadthFirstSearch(M));
+        
+        assertEquals(Algorithms.breadthFirstSearch(G2), Algorithms.breadthFirstSearch(M2));
     }
 
     @Test
     public void testDepthFirstSearch() {
-        G.addEdge(a, b);
-        G.addEdge(b, d);
-        G.addEdge(d, g);
-        G.addEdge(g, c);
-        G.addEdge(d, f);
-        G.addEdge(f, a);
-        G.addEdge(e, h);
-        G.addEdge(h, i);
-        G.addEdge(i, h);
-        G.addEdge(h, j);
 
-        // assertEquals(new HashSet<List<Vertex>>(),
-        // Algorithms.DepthFirstSearch(G));
         // assertEquals(new HashSet<List<Vertex>>(), Algorithms.depthFirstSearch(G));
+
         
-        M.addEdge(a, b);
-        M.addEdge(b, d);
-        M.addEdge(d, g);
-        M.addEdge(g, c);
-        M.addEdge(d, f);
-        M.addEdge(f, a);
-        M.addEdge(e, h);
-        M.addEdge(h, i);
-        M.addEdge(i, h);
-        M.addEdge(h, j);
         // assertEquals(new HashSet<List<Vertex>>(), Algorithms.depthFirstSearch(M));
+        
+        assertEquals(Algorithms.depthFirstSearch(G2), Algorithms.depthFirstSearch(M2));
     }
 
     @Test
     public void testCommonUpstreamVertices() {
-        G.addEdge(a, b);
-        G.addEdge(b, d);
-        G.addEdge(d, g);
-        G.addEdge(g, c);
-        G.addEdge(d, f);
-        G.addEdge(f, a);
-        G.addEdge(e, h);
-        G.addEdge(h, i);
-        G.addEdge(i, h);
-        G.addEdge(h, j);
         
-        //assertEquals(new ArrayList<Vertex>(), Algorithms.commonUpstreamVertices(G, f, g));
+        assertEquals(new ArrayList<Vertex>(Arrays.asList(f)), Algorithms.commonUpstreamVertices(G2, h, i));
+        assertEquals(new ArrayList<Vertex>(), Algorithms.commonUpstreamVertices(G2, b, c));
+
         
-        M.addEdge(a, b);
-        M.addEdge(b, d);
-        M.addEdge(d, g);
-        M.addEdge(g, c);
-        M.addEdge(d, f);
-        M.addEdge(f, a);
-        M.addEdge(e, h);
-        M.addEdge(h, i);
-        M.addEdge(i, h);
-        M.addEdge(h, j);
+        assertEquals(new ArrayList<Vertex>(Arrays.asList(f)), Algorithms.commonUpstreamVertices(M2, h, i));
+        assertEquals(new ArrayList<Vertex>(), Algorithms.commonUpstreamVertices(M2, b, c));
         
-        //assertEquals(new ArrayList<Vertex>(), Algorithms.commonUpstreamVertices(M, f, g));
     }
     
     public void testCommonDownstreamVertices() {
-        G.addEdge(a, b);
-        G.addEdge(b, d);
-        G.addEdge(d, g);
-        G.addEdge(g, c);
-        G.addEdge(d, f);
-        G.addEdge(f, a);
-        G.addEdge(e, h);
-        G.addEdge(h, i);
-        G.addEdge(i, h);
-        G.addEdge(h, j);
         
-        assertEquals(new ArrayList<Vertex>(), Algorithms.commonUpstreamVertices(G, f, g));
+        assertEquals(new ArrayList<Vertex>(Arrays.asList(a)), Algorithms.commonDownstreamVertices(G2, b, c));
+        assertEquals(new ArrayList<Vertex>(), Algorithms.commonDownstreamVertices(G2, h, i));
         
-        M.addEdge(a, b);
-        M.addEdge(b, d);
-        M.addEdge(d, g);
-        M.addEdge(g, c);
-        M.addEdge(d, f);
-        M.addEdge(f, a);
-        M.addEdge(e, h);
-        M.addEdge(h, i);
-        M.addEdge(i, h);
-        M.addEdge(h, j);
         
-        assertEquals(new ArrayList<Vertex>(), Algorithms.commonUpstreamVertices(M, f, g));
+        assertEquals(new ArrayList<Vertex>(Arrays.asList(a)), Algorithms.commonDownstreamVertices(M2, b, c));
+        assertEquals(new ArrayList<Vertex>(), Algorithms.commonDownstreamVertices(M2, h, i));
     }
 
 }
